ac81b602e3d1299bb9cb40190f63a7aa2743d2ea
Dani <dagugo.96@gmail.com>
2023-10-01 09:47:49 -0500

    [Feature]: update Unity version

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
-	[SerializeField]
-	private Text ScoreText;
-	[SerializeField]
-	private Text[] highScoreTexts;
+	// [SerializeField]
+	// private Text ScoreText;
+	// [SerializeField]
+	// private Text[] highScoreTexts;
 
 	private float highScore1;
     private float highScore2;
@@ -37,7 +36,7 @@ public class ScoreManager : MonoBehaviour {
 		if (scoreIncreasing) {
             ScoreIncreasing();
         }
-        ScoreText.text = "Score: " + Mathf.Round(scoreCount);
+        // ScoreText.text = "Score: " + Mathf.Round(scoreCount);
     }
 	public void AddScore(int pointsToAdd){
 		scoreCount += pointsToAdd;
@@ -50,28 +49,28 @@ public class ScoreManager : MonoBehaviour {
 
     public void NewHighScore()
     {
-        highScoreTexts[0].text = "High Score 1: " + Mathf.Round(highScore1);
-        highScoreTexts[1].text = "High Score 2: " + Mathf.Round(highScore2);
-        highScoreTexts[2].text = "High Score 3: " + Mathf.Round(highScore3);
+        // highScoreTexts[0].text = "High Score 1: " + Mathf.Round(highScore1);
+        // highScoreTexts[1].text = "High Score 2: " + Mathf.Round(highScore2);
+        // highScoreTexts[2].text = "High Score 3: " + Mathf.Round(highScore3);
         if (scoreCount > highScore1)
         {
             highScore1 = scoreCount;
             PlayerPrefs.SetFloat("HighScore1", highScore1);
-            highScoreTexts[0].text = "High Score 1: " + Mathf.Round(highScore1);
+            // highScoreTexts[0].text = "High Score 1: " + Mathf.Round(highScore1);
             scoreCount = 0;
         }
         if (scoreCount > highScore2)
         {
             highScore2 = scoreCount;
             PlayerPrefs.SetFloat("HighScore2", highScore2);
-            highScoreTexts[1].text = "High Score 2: " + Mathf.Round(highScore2);
+            // highScoreTexts[1].text = "High Score 2: " + Mathf.Round(highScore2);
             scoreCount = 0;
         }
         if (scoreCount > highScore3)
         {
             highScore3 = scoreCount;
             PlayerPrefs.SetFloat("HighScore3", highScore3);
-            highScoreTexts[2].text = "High Score 3: " + Mathf.Round(highScore3);
+            // highScoreTexts[2].text = "High Score 3: " + Mathf.Round(highScore3);
             scoreCount = 0;
         }
     }
